c3eb685a59fc6d2856cf51283198e12b5fcadd7d
System <none>
2017-04-07 17:08:23 +0000

    Checkpoint

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
 const twitter = require('twitter');
+const _ = require('lodash');
 
 //
 // This defines two routes that our API is going to use.
@@ -20,34 +21,31 @@ var routes = function(app) {
 // specified `"/"`.  For any GET request received at "/", we're sending some HTML back and logging the
 // request to the console. The HTML you see in the browser is what `res.send()` is sending back.
 //
+  
+  var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+  
   app.get("/", function(req, res) {
     res.send("<h1>REST API</h1><p>Oh, hi! There's not much to see here - view the code instead</p><footer id=\"gWidget\"></footer><script src=\"https://widget.glitch.me/widget.min.js\"></script>");
     console.log("Received GET");
   });
   
   app.post('/test-webhook', function(req, res) {
-    console.log('test-webhook:', req.body);
-    // { 'content-type': 'application/json',
-    //   'x-imdone-signature': '1f93b809debbebfe36c97386134e86ee08ef558b',
-    //   host: 'localhost:3000',
-    //   'content-length': '979',
-    //   connection: 'close' }
-
     // DOING: Integrate with twitter using [desmondmorris/node-twitter: Client library for the Twitter REST and Streaming API's.](https://github.com/desmondmorris/node-twitter) id:101
     // DOING: Integrate with trello using [adunkman/node-trello: Node wrapper for Trello's HTTP API.](https://github.com/adunkman/node-trello) id:102
 
-    console.log(req.headers);
-
     var signature = req.headers["x-imdone-signature"];
     getHMACDigest(JSON.stringify(req.body), function(digest) {
-      console.log("digest:", digest);
-      console.log("signature");
-      if (digest === signature) {
-        // req.body.taskNow.text += " +signature"
-        res.status(200).json(req.body.taskNow); // DOING: This should send the task back if it should be updated id:99
-      } else {
-        res.status(403);
+      if (digest !== signature) return res.status(403);
+
+      if (req.body.delta) {
+        
       }
+      res.status(200).json(req.body.taskNow); // DOING: This should send the task back if it should be updated id:99
     });
   });
 
